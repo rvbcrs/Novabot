37c39c0c79fcffc650d6256451c8f61cd9074cf5
Ramon van Bruggen <ramonvanbruggen@gmail.com>
2026-09-19 13:32:57 +0200

    build: release-app.sh also picks up commits with a bare app: prefix

--- release-app.sh
+++ release-app.sh
@@ -91,7 +91,8 @@ RELEASE_NOTES=$(
   git log --pretty=format:'%s' ${COMMIT_RANGE} 2>/dev/null \
     | grep -vE '^(release|chore|test|docs|ci|build|style|refactor)(\(|:)' \
     | grep -vE '^Merge ' \
-    | grep -E '\([^)]*(app|i18n|admin|firmware)[^)]*\)|^(fix|feat):' \
+    | grep -E '\([^)]*(app|i18n|admin|firmware)[^)]*\)|^(fix|feat):|^[a-z+]*(app|i18n)[a-z+]*:' \
+    | sed -E 's/^[a-z+]*(app|i18n)[a-z+]*:[[:space:]]+//' \
     | sed -E 's/^(fix|feat|perf)\([^)]+\):[[:space:]]+//' \
     | sed -E 's/^(fix|feat|perf):[[:space:]]+//' \
     | sed -E 's/[[:space:]]*\(#[0-9]+\)[[:space:]]*$//' \
